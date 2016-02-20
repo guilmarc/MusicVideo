cd4aa652aeec7e81dd85fb197f5190a176c524ec
Marco Guilmette <marco.guilmette@infologique.ca>
2016-02-20 06:34:08 -0500

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Marco Guilmette on 2016-02-20.
+//  Copyright © 2016 Marco Guilmette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
